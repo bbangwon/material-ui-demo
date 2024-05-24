4e1c8847a4c4f8af51dc73466eb34e74f08bcf40
youngil <youngil@placeb.com>
2024-05-24 19:14:50 +0900

    머터리얼 폼

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import AlarmIcon from '@mui/icons-material/Alarm';
 import RatingDemo from './RatingDemo';
+import FormDemo from './FormDemo';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       </IconButton>
 
       <RatingDemo />
+      <FormDemo />
     </div>
   )
 }
---
+++ src/FormDemo.jsx
@@ -0,0 +1,30 @@
+import TextField from "@mui/material/TextField"
+import Slider from "@mui/material/Slider"
+import { useState } from "react"
+
+export default function FormDemo() {
+    const [name, setName] = useState('');
+    const [volume, setVolume] = useState(0);
+    const updateName = (e) => {
+        setName(e.target.value);
+    }
+    const changeVolume = (event, newValue) => {
+        setVolume(newValue);
+    }
+    return (
+        <div>
+            <h1>Form Demo</h1>
+            <h2>너의 이름은 {name}</h2>
+            <TextField 
+                id="outlined-basic" 
+                placeholder="이름을 써라!"
+                label="이름" 
+                variant="outlined"
+                value={name} 
+                onChange={updateName}/>
+
+            <h2>Volume is {volume}</h2>
+            <Slider aria-label="Volume" value={volume} onChange={changeVolume} />                
+        </div>
+    )
+}
