880ffb641de47b2a7c51a308a7e90f2289e1f977
youngil <youngil@placeb.com>
2024-05-24 19:02:29 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import AlarmIcon from '@mui/icons-material/Alarm';
+
+function App() {
+  return (
+    <div>
+      <Button variant="text" onClick={() => alert('Hi!')}>Text</Button>
+      <Button color="success" variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button color="error" variant="contained">Contained</Button>
+      <Button color="secondary" variant="contained" size="large">Contained</Button>
+
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+    </div>
+  )
+}
+
+export default App
